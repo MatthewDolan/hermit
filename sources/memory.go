4225d93ae71845fdcbae33e11be4727f8061b550
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2022-09-14 09:16:13 -0700

    feat: allow `MemSource` to contain multiple packages

--- sources/memory.go
+++ sources/memory.go
@@ -1,6 +1,7 @@
 package sources
 
 import (
+	"fmt"
 	"io/fs"
 
 	"github.com/cashapp/hermit/ui"
@@ -8,26 +9,28 @@ import (
 )
 
 // MemSource is a new Source based on a name and content kept in memory
-type MemSource struct {
-	name    string
-	content string
-}
+type MemSource map[string]string
 
 // NewMemSource returns a new MemSource
-func NewMemSource(name, content string) *MemSource {
-	return &MemSource{name, content}
+func NewMemSource(name, content string) MemSource {
+	return MemSource{name: content}
 }
 
-func (s *MemSource) Sync(_ *ui.UI, _ bool) error { // nolint: golint
+func (s MemSource) Sync(_ *ui.UI, _ bool) error { // nolint: golint
 	return nil
 }
 
-func (s *MemSource) URI() string { // nolint: golint
-	return s.name
+func (s MemSource) URI() string { // nolint: golint
+	var names []string
+	for name := range s {
+		names = append(names, name)
+	}
+	if len(names) == 1 {
+		return names[0]
+	}
+	return fmt.Sprintf("%s", names)
 }
 
-func (s *MemSource) Bundle() fs.FS { // nolint: golint
-	return vfs.InMemoryFS(map[string]string{
-		s.name: s.content,
-	})
+func (s MemSource) Bundle() fs.FS { // nolint: golint
+	return vfs.InMemoryFS(s)
 }
